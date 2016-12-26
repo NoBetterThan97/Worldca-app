d09caeb5d148c59f0da9a385ce96663aba6e7a24
edsong1257 <admin98@gmail.com>
2016-12-26 09:57:39 +0800

    ui_v2

--- controllers/food.rb
+++ controllers/food.rb
@@ -8,21 +8,9 @@ class WorldcaApp < Sinatra::Base
   # Home page: show number of courses each resource contains
 
   get "/?" do
-    result = GetHighRank5.call
-    if result.success?
-      @data = RankView.new(result.value)
-    else
-      flash[:error] = result.value.message
-    end
 
-    result_daily = GetDailyConsumed.call
-    if result.success?
-      @data_Daily_Consumed = result_daily.value.worldcals
-    else
-      flash[:error] = result.value.message
-    end
+    slim :foods
 
-    slim :rank_high_5
   end
   get '/foods/:name/?' do
     #group_details = GetGroupDetails.call(params[:name])
