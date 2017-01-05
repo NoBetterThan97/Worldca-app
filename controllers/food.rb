0851a5e24281c427f2370bddab5da90e697cc861
LeoLee <leo.li@iss.nthu.edu.tw>
2017-01-05 14:17:14 +0800

    Remove less 25

--- controllers/food.rb
+++ controllers/food.rb
@@ -13,14 +13,6 @@ class WorldcaApp < Sinatra::Base
       flash[:error] = result.value.message
     end
 
-    criteria = RankCriteria.new(false, 5)
-    result = GetRank.call(criteria)
-    if result.success?
-      @data_less = RankView.new(result.value)
-    else
-      flash[:error] = result.value.message
-    end
-
     result_daily = GetDailyConsumed.call
     if result.success?
       @data_Daily_Consumed = result_daily.value.worldcals
@@ -30,7 +22,7 @@ class WorldcaApp < Sinatra::Base
 
     slim :foods
   end
-  get '/food/nutrix/:name/?' do
+  get '/foods/nutrix/:name/?' do
     result = GetFood.call(params[:name])
     if result.success?
       @data = result.value
@@ -42,16 +34,6 @@ class WorldcaApp < Sinatra::Base
   end
   get '/foods/top' do
     criteria = RankCriteria.new(true, 25)
-    result = GetRank.call(criteria)
-    if result.success?
-      @data = RankView.new(result.value)
-    else
-      flash[:error] = result.value.message
-    end
-    slim :rank
-  end
-  get '/foods/less' do
-    criteria = RankCriteria.new(false, 25)
     result = GetRank.call(criteria)
     if result.success?
       @data = RankView.new(result.value)
