dbae50ff9c4b46f7d75301206aae7ec903506e36
LeoLee <leo.li@iss.nthu.edu.tw>
2017-01-06 19:43:55 +0800

    Fix detail for api change

--- services/get_food.rb
+++ services/get_food.rb
@@ -5,8 +5,8 @@ class GetFood
   extend Dry::Monads::Either::Mixin
 
   def self.call(name)
-    results = HTTP.get("#{WorldcaApp.config.WORLDCA_API}/food/nutrix/#{name}")
-    Right(FoodDetailsRepresenter.new(Food_Details.new).from_json(results.body))
+    results = HTTP.get("#{WorldcaApp.config.WORLDCA_API}/foods/#{name}")
+    Right(FoodRepresenter.new(Food.new).from_json(results.body))
   rescue
     Left(Error.new('Our servers failed - we are investigating!'))
   end
